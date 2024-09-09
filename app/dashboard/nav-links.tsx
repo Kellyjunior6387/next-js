8834dba1a8873e5d4f6dff0e696430175f938390
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-09-09 15:41:48 +0300

    3rd commit

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -7,7 +7,6 @@ import {
   import clsx from 'clsx';
   import Link from 'next/link';
   import { usePathname } from 'next/navigation';   
-  // ...
    
   export default function NavLinks() {
     const pathname = usePathname();
